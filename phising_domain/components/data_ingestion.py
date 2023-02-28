6f415b207ce2ffeb96a6385c1098e816ccbcbf69
mahammadrafi <mahammadrafishaik222@gmail.com>
2023-02-28 10:07:00 +0530

    start of data ingestion

---
+++ phising_domain/components/data_ingestion.py
@@ -0,0 +1,14 @@
+import os,sys 
+from phising_domain.logger import logging 
+from phising_domain.exception import PhisingException 
+from phising_domain.entity import config_entity 
+import pandas as phsing_domaain
+import numpy as np
+from phising_domain import utils 
+from sklearn.model_selection import train_test_split
+
+
+
+class DataIngest
+
+
--- phising_domain/entity/config_entity.py
+++ phising_domain/entity/config_entity.py
@@ -1,4 +1,43 @@
-class DataIngestionConfig:...
+from phising_domain.logger import logging
+from phising_domain.exception import PhisingException 
+import os,sys
+from datetime import datetime
+
+
+
+FILE_NAME="phising.csv"
+TRAIN_FILE_NAME="train.csv"
+TEST_FILE_NAME="test.csv"
+
+class TrainingPipelineConfig:
+    def __init__(self):
+        try:
+            self.artifact_dir=os.path.join(os.getcwd(),'artifacts',f"{datetime.now().strftime('%d%m%Y__%H%M%S')}")
+        except Exception as e:
+            raise PhisingException(e, sys)
+
+
+
+class DataIngestionConfig:
+    def __init__(self,training_pipe_line:TrainingPipelineConfig):
+        try:
+            self.data_base_name="phsing_domaain"
+            self.collection_name="website"
+            self.data_ingestion_dir=os.path.join(training_pipe_line_config.artifact_dir,"data_ingestion")
+            self.feature_store_file_path=os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+            self.train_file_path=os.path.join(self.data_ingestion_dir,"data_set",TRAIN_FILE_NAME)
+            self.test_file_path=os.path.join(self.data_ingestion_dir,"data_set",TEST_FILE_NAME)
+            self.test_size=0.25
+        except Exception as e:
+            raise PhisingException(e, sys)
+
+    def to_dict(self):
+        try:
+            return self.__dict__ 
+        except Exception as e:
+            raise PhisingException(e, sys)
+
+
 class DataValidationConfig:...
 class DataTransformationConfig:...
 class ModelTrainerConfig:...
